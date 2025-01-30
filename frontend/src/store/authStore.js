a14313c038320375e1fabbe03efcbd88011f4ec5
JemenVan <pastorjemen@gmail.com>
2025-01-30 18:25:00 +0800

    Update authStore.js
    naka comment

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -275,19 +275,19 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-  // checkAuth: async () => {
-  //   set({ isCheckingAuth: true, error: null });
-  //   try {
-  //     const response = await axios.get(`${API_URL}/check-auth`);
-  //     set({
-  //       user: response.data.user,
-  //       isAuthenticated: true,
-  //       isCheckingAuth: false,
-  //     });
-  //   } catch (error) {
-  //     set({ error: null, isCheckingAuth: false, isAuthenticated: false });
-  //   }
-  // },
+  checkAuth: async () => {
+    set({ isCheckingAuth: true, error: null });
+    try {
+      const response = await axios.get(`${API_URL}/check-auth`);
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isCheckingAuth: false,
+      });
+    } catch (error) {
+      set({ error: null, isCheckingAuth: false, isAuthenticated: false });
+    }
+  },
 
   forgotPassword: async (email) => {
     set({ isLoading: true, error: null });
